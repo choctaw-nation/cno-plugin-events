727e677ae0fac7d4affc9c33dec1b4997b726b9f
KJ Roelke <kroelke@choctawnation.com>
2026-07-15 08:29:33 -0500

    fix tests

--- tests/test-event-handler.php
+++ tests/test-event-handler.php
@@ -79,6 +79,29 @@ class Test_Event_Handler extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Ensures an already created past event is set to draft when expiry runs.
+	 */
+	public function test_created_event_expires_on_method_call() {
+		$now     = new DateTimeImmutable( 'now', wp_timezone() );
+		$post_id = self::factory()->post->create(
+			array(
+				'post_type'   => $this->cpt,
+				'post_status' => 'publish',
+			)
+		);
+
+		$this->set_field( $post_id, 'event_details_time_and_date_is_all_day', true );
+		$this->set_field( $post_id, 'event_details_time_and_date_start_date', $now->modify( '-2 day' )->format( 'Y-m-d' ) );
+		$this->set_field( $post_id, 'event_details_time_and_date_end_date', $now->modify( '-1 day' )->format( 'Y-m-d' ) );
+
+		$this->assertSame( 'publish', get_post( $post_id )->post_status );
+
+		$this->handler->expire_choctaw_events();
+
+		$this->assertSame( 'draft', get_post( $post_id )->post_status );
+	}
+
+	/**
 	 * Data provider with different event field combinations and expected outcomes.
 	 *
 	 * @return array[]
@@ -92,78 +115,78 @@ class Test_Event_Handler extends WP_UnitTestCase {
 		return array(
 			'past all day event should expire'          => array(
 				array(
-					'is_all_day' => true,
-					'start_date' => $past_start_date,
-					'end_date'   => $past_end_date,
+					'event_details_time_and_date_is_all_day' => true,
+					'event_details_time_and_date_start_date' => $past_start_date,
+					'event_details_time_and_date_end_date' => $past_end_date,
 				),
 				'draft',
 			),
 			'future all day event should be published'  => array(
 				array(
-					'is_all_day' => true,
-					'start_date' => $future_start_date,
-					'end_date'   => $future_end_date,
+					'event_details_time_and_date_is_all_day' => true,
+					'event_details_time_and_date_start_date' => $future_start_date,
+					'event_details_time_and_date_end_date' => $future_end_date,
 				),
 				'publish',
 			),
 			'current all day event w/o time should be published' => array(
 				array(
-					'is_all_day' => true,
-					'start_date' => $now->format( 'Y-m-d' ),
-					'end_date'   => '',
+					'event_details_time_and_date_is_all_day' => true,
+					'event_details_time_and_date_start_date' => $now->format( 'Y-m-d' ),
+					'event_details_time_and_date_end_date' => '',
 				),
 				'publish',
 			),
 			'future event should be published'          => array(
 				array(
-					'is_all_day' => false,
-					'start_date' => $future_start_date,
-					'end_date'   => $future_end_date,
+					'event_details_time_and_date_is_all_day' => false,
+					'event_details_time_and_date_start_date' => $future_start_date,
+					'event_details_time_and_date_end_date' => $future_end_date,
 				),
 				'publish',
 			),
 			'past event without end date should expire' => array(
 				array(
-					'is_all_day' => false,
-					'start_date' => $past_start_date,
-					'end_date'   => '',
-					'end_time'   => '',
+					'event_details_time_and_date_is_all_day' => false,
+					'event_details_time_and_date_start_date' => $past_start_date,
+					'event_details_time_and_date_end_date' => '',
+					'event_details_time_and_date_end_time' => '',
 				),
 				'draft',
 			),
 			'past event with time < now should expire'  => array(
 				array(
-					'is_all_day' => false,
-					'start_date' => $past_start_date,
-					'end_date'   => $now->format( 'Y-m-d' ),
-					'end_time'   => '00:00',
+					'event_details_time_and_date_is_all_day' => false,
+					'event_details_time_and_date_start_date' => $past_start_date,
+					'event_details_time_and_date_end_date' => $now->format( 'Y-m-d' ),
+					'event_details_time_and_date_end_time' => '00:00',
 				),
 				'draft',
 			),
 			'past event with time == now should expire' => array(
 				array(
-					'is_all_day' => false,
-					'start_date' => $past_start_date,
-					'end_date'   => $now->format( 'Y-m-d' ),
-					'end_time'   => $now->format( 'H:i' ),
+					'event_details_time_and_date_is_all_day' => false,
+					'event_details_time_and_date_start_date' => $past_start_date,
+					'event_details_time_and_date_end_date' => $now->format( 'Y-m-d' ),
+					'event_details_time_and_date_end_time' => $now->format( 'H:i' ),
 				),
 				'draft',
 			),
 			'current event with no end time should expire if now < start time +1 hr' => array(
 				array(
-					'is_all_day' => false,
-					'start_date' => $now->modify( '-2 hour' )->format( 'Y-m-d' ),
-					'end_date'   => $now->format( 'Y-m-d' ),
-					'end_time'   => '',
+					'event_details_time_and_date_is_all_day' => false,
+					'event_details_time_and_date_start_date' => $now->modify( '-2 hour' )->format( 'Y-m-d' ),
+					'event_details_time_and_date_end_date' => $now->format( 'Y-m-d' ),
+					'event_details_time_and_date_end_time' => '',
 				),
 				'draft',
 			),
 			'ongoing event that ends in the future should be published' => array(
 				array(
-					'is_all_day' => false,
-					'start_date' => $past_start_date,
-					'start_time' => $now->format( 'H:i' ),
-					'end_date'   => $future_end_date,
+					'event_details_time_and_date_is_all_day' => false,
+					'event_details_time_and_date_start_date' => $past_start_date,
+					'event_details_time_and_date_start_time' => $now->format( 'H:i' ),
+					'event_details_time_and_date_end_date' => $future_end_date,
 				),
 				'publish',
 			),
